c07ea654ad94a88a7c4c6257f1d8b3fafaba68ed
Divixity <56671866+Divixity@users.noreply.github.com>
2022-02-07 15:08:54 +0100

    Class for loading .pkl

---
+++ DataRetriever.py
@@ -0,0 +1,45 @@
+import configparser
+import os
+import pandas as pd
+
+import main
+
+
+class DataRetriever:
+    """
+    Class to locate, and retrive data, as .pkl, stored in a pd.DataFrame.
+    """
+    def __init__(self):
+        self.__config_path = os.path.join(main.root, 'Config', 'database_path.ini')
+        self.__data_path = self.__config_load()
+
+    def __config_load(self):
+        """
+        Private method to read config file for path to OneDrive.
+        :return: Path.
+        """
+        config = configparser.ConfigParser()
+
+        config.read(self.__config_path)
+
+        return config['database']['directory']
+
+    def get_data(self, file_name):
+        """
+        Method for reading a pickle file, given a file name, and returning it as a pd.DataFrame.
+        :param file_name: File which to open as a pd.DataFrame
+        :return: pd.DataFrame of the pickle file.
+        """
+        path = os.path.join(self.__data_path, file_name)
+
+        df = pd.read_pickle(path)
+
+        return df
+
+
+if __name__ == "__main__":
+    open_data = DataRetriever()
+
+    df = open_data.get_data("All-Subsystems-hour-Year1.pkl")
+
+    print(df.head(10))
